83038b5a48509fe16dcab6933b95a4b3f83cd00b
shivasharma14 <shamashiva100s@gmail.com>
2025-06-20 21:29:48 +0530

    Added the API call

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,61 +1,54 @@
-import { useState } from "react"
+import { useState } from "react";
+import IngredientsList from "./components/IngredientsList";
+import { getRecipeFromMistral } from "./ai";
+import ClaudeRecipe from "./components/ClaudeRecipe";
 
 export default function Content() {
-    const [ingredients, setIngredients] = useState<string[]>([]);
-    const [recipeShown, setRecipeShown] = useState<boolean>(false);
+  const [ingredients, setIngredients] = useState<string[]>([]);
+  const [recipe, setRecipe] = useState<string>("");
 
-    function toggleRecipeShown(){
-        setRecipeShown(prevShown => !prevShown)
+  async function getRecipe() {
+    const recipeMarkdown = (await getRecipeFromMistral(ingredients)) as string;
+    //console.log(recipeMarkdown)
+    setRecipe(recipeMarkdown);
+  }
+
+  // This is if we are using the onSubmit inside the form in place of the action (we need to explicitly prevent defaults here)
+
+  // function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+  //     event.preventDefault();
+  //     const formData = new FormData(event.currentTarget);
+  //     const newIngredient = formData.get("ingredient");
+
+  //     if (typeof newIngredient === "string" && newIngredient.trim() !== "") {
+  //         setIngredients(prevIngredients => [...prevIngredients, newIngredient]);
+  //     }
+  // }
+
+  function handleSubmit(formData: any) {
+    const newIngredient = formData.get("ingredient");
+
+    if (typeof newIngredient === "string" && newIngredient.trim() !== "") {
+      setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
     }
+  }
 
-    const ingredientsList = ingredients.map((ingredient) => (
-        <li key={ingredient}>{ingredient}</li>
-    ));
+  return (
+    <main>
+      <form action={handleSubmit} className="add-ingredient-form">
+        <input
+          type="text"
+          placeholder="e.g. oregano"
+          aria-label="Add ingredient"
+          name="ingredient"
+        />
+        <button>Add ingredient</button>
+      </form>
+      {ingredients.length > 0 && (
+        <IngredientsList ingredients={ingredients} getRecipe={getRecipe} />
+      )}
 
-    // This is if we are using the onSubmit inside the form in place of the action (we need to explicitly prevent defaults here)
-
-    // function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    //     event.preventDefault();
-    //     const formData = new FormData(event.currentTarget);
-    //     const newIngredient = formData.get("ingredient");
-
-    //     if (typeof newIngredient === "string" && newIngredient.trim() !== "") {
-    //         setIngredients(prevIngredients => [...prevIngredients, newIngredient]);
-    //     }
-    // }
-
-    function handleSubmit(formData: any){
-        const newIngredient = formData.get("ingredient");
-
-        if (typeof newIngredient === "string" && newIngredient.trim() !== "") {
-            setIngredients(prevIngredients => [...prevIngredients, newIngredient]);
-        }
-    }
-
-    return (
-        <main>
-            <form action={handleSubmit} className="add-ingredient-form">
-                <input 
-                    type="text"
-                    placeholder="e.g. oregano"
-                    aria-label="Add ingredient"
-                    name="ingredient"
-                />
-                <button>Add ingredient</button>
-            </form>
-            { ingredients.length > 0 && <section>
-                <h2>Ingredients on hand:</h2>
-                <ul className="ingredients-list" aria-live="polite">
-                    {ingredientsList}
-                </ul>
-                {ingredients.length>3 && <div className="get-recipe-container">
-                    <div>
-                        <h3>Ready for a recipe?</h3>
-                        <p>Generate a recipe from your list of ingredients.</p>
-                    </div>
-                    <button>Get a recipe</button>
-                </div>}
-            </section> }
-        </main>
-    );
+      {recipe && <ClaudeRecipe recipe={recipe}/>}
+    </main>
+  );
 }
